49b50f5bb7ef586a8c1a8bb0d0694daaf1996ecc
nelson <nelsonponce00@outlook.com>
2022-08-08 12:58:25 +0000

    nuevos cambios prueba 1

---
+++ ex1_calificatudia.py
@@ -0,0 +1,11 @@
+
+
+# ---- Califica tu día ----
+
+print("Que tal fue tu día?? Mejor que ayer?")
+diaAyer = int(input("Que tal tu día ayer: "))
+diaHoy = int(input("Que tal tu día hoy: "))
+mediaDia= (diaHoy + diaAyer)/2
+
+print("Tu día hoy fue: ", diaHoy, "", "Tu día ayer fue: ",diaAyer)
+print(mediaDia)
