fc8000fd217e31d04c19f47c5cc84404a03ff001
nelson <nelsonponce00@outlook.com>
2022-08-08 13:00:19 +0000

    prueba2

--- ex1_calificatudia.py
+++ ex1_calificatudia.py
@@ -7,5 +7,8 @@ diaAyer = int(input("Que tal tu día ayer: "))
 diaHoy = int(input("Que tal tu día hoy: "))
 mediaDia= (diaHoy + diaAyer)/2
 
+print("prueba prueba prueba")
+
+
 print("Tu día hoy fue: ", diaHoy, "", "Tu día ayer fue: ",diaAyer)
 print(mediaDia)
